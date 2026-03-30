f7f65f8eefb6a9336c3d1f3abe248c03b8d16e8b
codexuz <examonlineedumo@gmail.com>
2026-03-30 13:37:43 +0500

    "main"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
+  telemetry: { enabled: false },
+   devServer: {
+    host: '0',
+  },
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  },
+  // Avoids error [unhandledRejection] EMFILE: too many open files, watch
+  ignore: ['**/src-tauri/**'],
   modules: ["@nuxt/ui", "nuxt-echarts"],
   css: ["~/assets/css/tailwind.css"],
   echarts: {
