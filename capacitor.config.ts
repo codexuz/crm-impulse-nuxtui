c3be8226169b07c2701cb3be9be38b88d85b1b4b
codexuz <examonlineedumo@gmail.com>
2026-04-08 15:25:24 +0500

    "main"

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'crm.impulsecrm.uz',
   appName: 'IMPULSE CRM',
   webDir: '.output/public',
-  bundledWebRuntime: false,
   plugins: {
     CapacitorHttp: {
       enabled: true,
